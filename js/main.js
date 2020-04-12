9d7625bb4c675b4f916ed0602a9006d6d0087b98
Dmitriy <57505659+kurochkin-dev@users.noreply.github.com>
2020-04-12 19:52:18 +0300

    added three sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$(function(){
-
-
-
-  
+$(document).ready(function () {
+    $('.reviews__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/prevarr.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/nextarr.png"</button>'
+    });
+    
 });
+
